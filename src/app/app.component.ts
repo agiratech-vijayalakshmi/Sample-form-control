6e38afa9023f7b7a2dbc60e44f03e0291346bf6a
viji <vijayalakshmi.r@agiratech.com>
2022-07-11 18:51:08 +0530

    Added Form control task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, MinLengthValidator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mobile_otp';
+  mobilenumber : FormControl;
+
+  constructor(){
+    this.mobilenumber = new FormControl('', [Validators.required, Validators.minLength(10)]);
+  }
+
+
+  submit(){
+    console.log(this.mobilenumber.value);
+  }
 }
